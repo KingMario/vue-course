d815d73b96c8f2cd4945c10389b436ea6186f480
lji <12>
2017-03-29 23:25:31 +0800

    123

---
+++ Lesson3-4/CompanyTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <span><company-logo :logo="logo"></company-logo><span class="slogan">{{slogan}}</span></span>
+</template>
+<script>
+import CompanyLogo from './CompanyLogo.vue'
+
+export default {
+  props: ['companyName', 'logo', 'slogan'],
+  components: {
+    CompanyLogo
+  }
+}
+</script>
+<style scoped>
+span.slogan {
+  display: block;
+  font-size: 12px;
+  color: #999;
+  margin-top: 20px;
+}
+</style>
---
+++ Lesson3-4/main.js
@@ -0,0 +1,9 @@
+var CompanyTitle = require('./CompanyTitle.vue')
+var Vue = require('vue')
+
+new Vue({
+  el: 'body',
+  components: {
+    CompanyTitle
+  }
+})
